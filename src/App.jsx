93976aef2375190a2c6d53de98be7412bc461e46
mpadilhaa <mpadilhaaac@gmail.com>
2023-12-16 14:28:35 -0300

    feat:create call api pexels

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,35 @@ import { useState, useRef } from "react";
 import "./App.css";
 
 import TemperatureDetails from "./components/TemperatureDetails/TemperatureDetails";
-import { apiWeatherCall } from "./utils/useCallApi";
+import { apiWeatherCall, apiPexelsCall } from "./utils/useCallApi";
 import WeatherInput from "./components/WeatherInput/WeatherInput";
 
 function App() {
   const [data, setData] = useState([]);
+  const [photo, setPhoto] = useState([]);
+
   const cityName = useRef(null);
 
   function submitForm(e) {
     e.preventDefault();
     apiWeatherCall(cityName.current?.value).then((data) => setData(data));
-    console.log(cityName);
+    apiPexelsCall(cityName.current?.value, setPhoto);
     console.log(data);
+    console.log(photo);
   }
   return (
     <>
       {data ? (
         <div className="w-body">
-          <img className="img-background" alt="" />
+          <img
+            className="img-background"
+            alt=""
+            src={photo.photos[0].src.landscape}
+          />
           <WeatherInput fnChangedValue={cityName} submitForm={submitForm} />
           <div className="info-weather">
             <div className="city-and-icon">
-              <h1 className="city">,</h1>
+              <h1 className="city">{data.name},</h1>
             </div>
             <div className="time-and-day">
               <h1 className="day">
@@ -33,7 +40,9 @@ function App() {
             </div>
             <div className="temperature">
               <div className="temperature-celcius">
-                <h1 className="temperature-value"></h1>
+                <h1 className="temperature-value">
+                  {data.main.temp.toFixed(0)}
+                </h1>
                 <span className="temperature-type">°C</span>
               </div>
               <div className="min-and-max">
--- src/utils/useCallApi.jsx
+++ src/utils/useCallApi.jsx
@@ -1,3 +1,5 @@
+import { createClient } from "pexels";
+
 export async function apiWeatherCall(city) {
   const response = await fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=8b32c61e6740d29e46f5ac6611825048&units=metric&lang=pt_br`
@@ -6,4 +8,13 @@ export async function apiWeatherCall(city) {
   const detailsWeather = await response.json();
 
   return detailsWeather;
+}
+
+export async function apiPexelsCall(city, photo) {
+  const client = createClient(
+    "m9GXfnNt6RRq8219EtFZxevyIBTXPholawTumBti0QkG3JmbNWxu4SWX"
+  );
+  const query = city;
+
+  client.photos.search({ query, per_page: 3 }).then((photos) => photo(photos));
 }
