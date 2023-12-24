fd4bda8f87d4ba74e63666f05527ce89afd3127d
mpadilhaa <mpadilhaaac@gmail.com>
2023-12-24 16:00:05 -0300

    feat: finish css mobile

--- src/components/WeatherInput/WeatherInput.jsx
+++ src/components/WeatherInput/WeatherInput.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import "./Input.css";
+import { IoSearchSharp } from "react-icons/io5";
 
 const WeatherInput = ({ fnChangedValue, submitForm }) => {
   return (
-    <form className="search-form" onSubmit={submitForm}>
-      <input className="search" type="text" ref={fnChangedValue} />
-    </form>
+    <div className="form-search">
+      <form className="form" onSubmit={submitForm}>
+        <input className="search" type="text" ref={fnChangedValue} />
+        <div className="icon-search">
+          <IoSearchSharp />
+        </div>
+      </form>
+    </div>
   );
 };
 
